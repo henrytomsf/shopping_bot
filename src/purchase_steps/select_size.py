70a39977ec4a306e72874e6166f816bfb39fb13c
Henry Tom <henry.tom.sf@gmail.com>
2022-07-23 23:53:54 -0700

    select size working again

--- src/purchase_steps/select_size.py
+++ src/purchase_steps/select_size.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
 from src.purchase_steps.steps_base import PurchaseStepBase
 from src.driver import WebDriver
 
@@ -13,8 +16,11 @@ class SelectSize(PurchaseStepBase):
 
     def run(self) -> bool:
         try:
-            self.driver.find_element_by_id(self.size).click()
+            element = self.driver.find_element(By.XPATH, "//*[@id='{}']".format(self.size))
+            ActionChains(self.driver).move_to_element(element).perform()
+            ActionChains(self.driver).click(element).perform()
             return True
+
         except Exception as e: #TODO add actual exception
             print('Click did not work.')
             print(e)
